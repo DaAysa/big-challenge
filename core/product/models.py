bfd241b08b30c873e9c79db766bba7608994f137
Daiana Aysa <da.aysa@gmail.com>
2021-11-16 14:10:33 -0300

    Added product model and admin view

--- core/product/models.py
+++ core/product/models.py
@@ -1,11 +1,9 @@
 from django.db import models
 
-class Category(models.Model):
+class Product(models.Model):
   name = models.CharField(max_length=256)
   image = models.URLField(max_length=256)
+  category = models.ForeignKey('category.Category', on_delete=models.CASCADE)
 
   def __str__(self):
     return self.name
-
-  class Meta:
-    verbose_name_plural = 'categories'
