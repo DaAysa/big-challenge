6e4bc610d0f55571cd2e987dd3a45b3be869ef90
Daiana Aysa <da.aysa@gmail.com>
2021-11-16 13:33:06 -0300

    Added Category attributes and admin view Reorganized files

--- core/category/models.py
+++ core/category/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+  class Meta:
+    verbose_name_plural = 'categories'
+
+  name = models.CharField(max_length=256)
+  image = models.URLField(max_length=256)
+
+  def __str__(self):
+    return self.name
