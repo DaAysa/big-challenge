8b14e9bc5e54cc676df4c9b2d764e61280f78711
Daiana Aysa <da.aysa@gmail.com>
2021-11-17 16:16:29 -0300

    Added Order model and api

--- core/api/urls.py
+++ core/api/urls.py
@@ -3,10 +3,12 @@ from rest_framework import routers
 
 from api.views.category import CategoryViewSet
 from api.views.product import ProductViewSet
+from api.views.order import OrderViewSet
 
 router = routers.DefaultRouter()
 router.register(r'categories', CategoryViewSet)
 router.register(r'products', ProductViewSet)
+router.register(r'orders', OrderViewSet)
 
 urlpatterns = [
   path('', include(router.urls)),
--- core/core/settings.py
+++ core/core/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     # Local apps
     'category',
     'product',
+    'order'
 ]
 
 MIDDLEWARE = [
